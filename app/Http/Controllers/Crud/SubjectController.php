24deec0a8dff46d9e61afef1b9a6e6f51347b630
= <=>
2024-03-19 00:41:36 +0300

    migration, models

---
+++ app/Http/Controllers/Crud/SubjectController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Crud;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SubjectController extends Controller
+{
+    public function addSubject()
+    {
+        return 'uyama';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 
 use App\Http\Controllers\Crud\StudentController;
-use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\Crud\SubjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',function(Request $request){
+Route::get('/',function(){
     return view('layout.master');
 });
 Route::get('/student/create',[StudentController::class,'create'])->name('student.create');
@@ -13,4 +13,4 @@ Route::post('/student/store',[StudentController::class,'store'])->name('student.
 Route::get('/student/list',[StudentController::class,'list_student'])->name('student.list');
 
 #subjects
-Route::get('/create-subject',[SubjectController::class,'create_subject'])->name('subject.add');
+Route::get('/add-subject',[SubjectController::class,'addSubject'])->name('subject.add');
